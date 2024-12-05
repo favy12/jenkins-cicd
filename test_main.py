14eefd9ead43fd607373f6e5ead5f8fd5715ab3a
Favour Makanju <makanjufavour@gmail.com>
2024-12-05 20:26:36 +0100

    Updated changes

--- test_main.py
+++ test_main.py
@@ -1,9 +1,11 @@
 import pytest
 from main import factorial
 
+
 def test_factorial_positive():
     assert factorial(5) == 120
     assert factorial(0) == 1
+
 
 def test_factorial_negative():
     with pytest.raises(ValueError):
